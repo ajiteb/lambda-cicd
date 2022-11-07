501161ba326ef9515d23585568d838807d2cdadb
Ajit Bansode <ajit@secfi.com>
2022-11-07 11:09:56 +0100

    feat: vault with kv store

--- vault-test/config.js
+++ vault-test/config.js
@@ -1,8 +1,8 @@
 const fs = require("fs");
 var vaultSecret = JSON.parse(fs.readFileSync("/tmp/secret.json"));
 module.exports = {
-  DB_CONFIG: {
-    user: vaultSecret.data.username,
-    password: vaultSecret.data.password,
-  },
+  // DB_CONFIG: {
+  //   user: vaultSecret.data.username,
+  //   password: vaultSecret.data.password,
+  // },
 };
--- vault-test/handler.js
+++ vault-test/handler.js
@@ -1,9 +1,13 @@
 'use strict';
 
 const config = require("./config");
-console.log(config.DB_CONFIG);
+// console.log(config.DB_CONFIG);
 
 module.exports.testVault = event => {
+  fs.readFile("/tmp/secret.json", (err, data) => {
+    if (err) throw err;
+    console.log(data.toString());
+ });
   return {
     statusCode: 200,
     body: JSON.stringify(
